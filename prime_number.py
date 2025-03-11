cbcfadc3ce4813aab162e1c63588ecec814eda44
ogush1 <s.oguro.24.11.1609@4nonome.com>
2025-03-11 21:29:41 +0900

    制御構文の解答

--- prime_number.py
+++ prime_number.py
@@ -2,3 +2,26 @@ a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
 # TODO
+q = int(a) - 1
+
+while True:
+    if q == 1:
+        print(a + "は素数である")
+        break
+    elif int(a) % q == 0:
+        print(a + "は素数でない")
+        break
+    else:
+        q -= 1
+
+q = int(b) - 1
+
+while True:
+    if q == 1:
+        print(b + "は素数である")
+        break
+    elif int(b) % q == 0:
+        print(b + "は素数でない")
+        break
+    else:
+        q -= 1
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,3 +3,14 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
+from math import pi
+a = 0
+b = pi / 2
+N = 100
+h = (b - a) / N
+k = 1
+S = 0
+
+for k in range(N + 1):
+    S += (h/2) * (sin(a + (k - 1) * h) + sin(a + k * h))
+print("台形の面積は" + str(S))
